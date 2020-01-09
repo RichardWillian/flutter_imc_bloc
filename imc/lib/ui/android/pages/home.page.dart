bd7ff3e09f760fd5a360aa840ffa0f903f00a29c
Richard Willian Ribeiro Divino <richard.divino@useargo.com>
2020-01-09 17:14:05 -0300

    Faz criação de interfaces, pastas e arquivos de cada projeto, tanto android quanto iOS

---
+++ imc/lib/ui/android/pages/home.page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cálculo IMC"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+                decoration: InputDecoration(labelText: "Altura (cm)"),
+                keyboardType: TextInputType.number),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+                decoration: InputDecoration(labelText: "Peso (kg)"),
+                keyboardType: TextInputType.number),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text("Você está fora de forma", textAlign: TextAlign.center),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: FlatButton(
+              child: Text("Calcular"),
+              color: Theme.of(context).primaryColor,
+              colorBrightness: Brightness.dark,
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
